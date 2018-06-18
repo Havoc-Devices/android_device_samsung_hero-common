1c7a47c304e874ab64310285f7d39baf0fcf98f5
Dan <daniel.pegg7849@gmail.com>
2018-06-18 14:34:33 +0000

    Change paths to include BSP binaries

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,4 +67,4 @@ BOARD_SECCOMP_POLICY += device/samsung/hero-common/seccomp
 
 # Inherit from the proprietary version
 -include vendor/samsung/hero-common/BoardConfigVendor.mk
--include vendor/seal/seal/BoardConfigVendor.mk
+-include vendor/samsung/hero-bsp/BoardConfigVendor.mk
